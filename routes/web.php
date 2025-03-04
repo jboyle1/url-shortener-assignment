b6fadb8150c6d49d1eb2f9bd2903e6533f24db27
Paddy <test@tests-MacBook-Pro.local>
2025-03-04 13:11:52 +0000

    Add routes for URL encoding, decoding, and redirection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlShortenerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/encode', [UrlShortenerController::class, 'encode']);
+Route::get('/{code}', [UrlShortenerController::class, 'redirect']);  // Redirection route
+Route::get('/decode/{code}', [UrlShortenerController::class, 'decode']);
